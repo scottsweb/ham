57dfc6735e383d303a4e82cef123df68be5967c0
Scott Evans <git@scott.ee>
2019-07-21 13:32:32 +0100

    Tweak rsync jobs

--- utilities/cron-backup-oak.sh
+++ utilities/cron-backup-oak.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -vzrl --delete --progress --exclude 'Containers' --exclude 'lost+found' --exclude 'Sort' /media/oak/Media/ /media/oak/Backup/
+rsync -vrl --delete --progress --update --exclude 'Containers' --exclude 'lost+found' --exclude 'Sort' /media/oak/Media/ /media/oak/Backup/
