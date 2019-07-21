c1a9d752e4b8f2ddb6d4b9b1813816f1e409b661
Scott Evans <git@scott.ee>
2019-07-21 13:57:22 +0100

    Only sync modified or new files

--- utilities/cron-container-sync.sh
+++ utilities/cron-container-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -vzrl --delete --progress --exclude 'homeassistant/.storage/auth' --exclude 'miniflux/db' --exclude 'plex' --exclude 'pihole/list.*' /media/oak/Bonsai/Containers/ /media/oak/Media/Containers/
+rsync -vzrl --delete --progress --update --exclude 'homeassistant/.storage/auth' --exclude 'miniflux/db' --exclude 'plex' --exclude 'pihole/list.*' /media/oak/Bonsai/Containers/ /media/oak/Media/Containers/
