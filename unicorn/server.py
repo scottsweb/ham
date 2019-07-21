dbd192b47b7d036e47740601b0ccf4d5439fbebd
Scott Evans <git@scott.ee>
2019-07-21 13:10:19 +0100

    Tinkering with bed sheets

--- unicorn/server.py
+++ unicorn/server.py
@@ -470,7 +470,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.11.2", 1883, 60)
+client.connect("192.168.11.3", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
