27d8edad20474ae79d832b27f4ed8b2a54589b5f
Scott Evans <git@scott.ee>
2019-08-25 18:12:32 +0100

    Further mycroft tweaks

--- mycroft/startup.sh
+++ mycroft/startup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-/opt/mycroft/./start-mycroft.sh all
+/opt/mycroft/./start-mycroft.sh bus && sleep 15
+/opt/mycroft/./start-mycroft.sh skills && sleep 15
+/opt/mycroft/./start-mycroft.sh voice && sleep 15
+/opt/mycroft/./start-mycroft.sh audio && sleep 15
+/opt/mycroft/./start-mycroft.sh enclosure
 tail -f /opt/mycroft/scripts/logs/mycroft-skills.log
