b329fc02d17ff7de628c75654010b759f228f394
Scott Evans <git@scott.ee>
2019-08-26 16:49:37 +0100

    Add dockermon for oak and implement controls for warrior

--- utilities/cron-backup.sh
+++ utilities/cron-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -vrl --delete --progress --exclude 'homeassistant/.storage/auth' --exclude 'miniflux/db' --exclude 'plex' --exclude 'pihole/list.*' /media/data/Containers/ /media/backup/Containers/
+rsync -vrl --delete --progress --exclude 'homeassistant/.storage/auth' --exclude 'miniflux/db' --exclude 'plex' --exclude 'warrior/data' --exclude 'pihole/list.*' /media/data/Containers/ /media/backup/Containers/
