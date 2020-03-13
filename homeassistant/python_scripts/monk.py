ecd07a9439ff00d0952bdc9ea6b230dcad58865f
Scott Evans <git@scott.ee>
2020-03-13 14:40:08 +0100

    Update cleaning zones due to re-map

--- homeassistant/python_scripts/monk.py
+++ homeassistant/python_scripts/monk.py
@@ -8,17 +8,17 @@ gemma = data.get("input_boolean.monk_clean_office_gemma")
 scott = data.get("input_boolean.monk_clean_office_scott")
 
 if lounge == 'on':
-  args["params"].extend([18])
-if kitchen == 'on':
   args["params"].extend([17])
+if kitchen == 'on':
+  args["params"].extend([19])
 if bedroom == 'on':
   args["params"].extend([16])
 if hall == 'on':
-  args["params"].extend([19])
-if gemma == 'on':
   args["params"].extend([20])
-if scott == 'on':
+if gemma == 'on':
   args["params"].extend([21])
+if scott == 'on':
+  args["params"].extend([18])
 
 #logger.info( args )
 hass.services.call('vacuum', 'send_command', args)
