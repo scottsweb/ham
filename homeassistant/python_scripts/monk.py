9c7c48d1007fb515f6866b0592ce515cccd2d885
Scott Evans <git@scott.ee>
2021-04-05 19:40:54 +0200

    More Monk room changes

--- homeassistant/python_scripts/monk.py
+++ homeassistant/python_scripts/monk.py
@@ -8,17 +8,17 @@ bathroom = data.get("input_boolean.monk_clean_bathroom")
 office = data.get("input_boolean.monk_clean_office")
 
 if lounge == 'on':
-  args["params"].extend([20])
-if kitchen == 'on':
-  args["params"].extend([15])
-if bedroom == 'on':
-  args["params"].extend([22])
-if dining == 'on':
   args["params"].extend([19])
-if bathroom == 'on':
+if kitchen == 'on':
   args["params"].extend([16])
-if office == 'on':
+if bedroom == 'on':
+  args["params"].extend([1])
+if dining == 'on':
   args["params"].extend([18])
+if bathroom == 'on':
+  args["params"].extend([17])
+if office == 'on':
+  args["params"].extend([21])
 
 #logger.info( args )
 hass.services.call('vacuum', 'send_command', args)
