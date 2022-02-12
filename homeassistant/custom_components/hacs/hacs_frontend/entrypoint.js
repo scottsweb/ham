c52828415d40d188a71dd025d5af3142057365e6
Scott Evans <git@scott.ee>
2022-02-12 21:45:28 +0100

    HACS update

--- homeassistant/custom_components/hacs/hacs_frontend/entrypoint.js
+++ homeassistant/custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,10 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-8113c4f2.js')")();
+  new Function("import('/hacsfiles/frontend/main-c4420e0b.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-8113c4f2.js';
+  el.src = '/hacsfiles/frontend/main-c4420e0b.js';
+  el.type = 'module';
   document.body.appendChild(el);
 }
   
